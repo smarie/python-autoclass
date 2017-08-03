6e806468b1035a2782c94e9af67662a54353fe92
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-08-04 01:23:21 +0200

    * improving coverage by commenting out unused methods

--- classtools_autocode/autoargs.py
+++ classtools_autocode/autoargs.py
@@ -236,47 +236,47 @@ def get_constructor(item_type):
     return constructor
 
 
-def get_class_that_defined_method(meth):
-    """
-    Utility method, from http://stackoverflow.com/questions/3589311/get-defining-class-of-unbound-method-object-in-python-3/25959545#25959545
+# def get_class_that_defined_method(meth):
+#     """
+#     Utility method, from http://stackoverflow.com/questions/3589311/get-defining-class-of-unbound-method-object-in-python-3/25959545#25959545
+#
+#     :param meth:
+#     :return:
+#     """
+#
+#     if ismethod(meth):
+#         # this won't happen in python 3.5 : https://bugs.python.org/issue27901
+#         for cls in getmro(meth.__self__.__class__):
+#            if cls.__dict__.get(meth.__name__) is meth:
+#                 return cls
+#         meth = meth.__func__ # fallback to __qualname__ parsing
+#     if isfunction(meth):
+#         cls = getattr(getmodule(meth),
+#                       meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
+#         if isinstance(cls, type):
+#             return cls
+#     return None # not required since None would have been implicitly returned anyway
 
-    :param meth:
-    :return:
-    """
 
-    if ismethod(meth):
-        # this won't happen in python 3.5 : https://bugs.python.org/issue27901
-        for cls in getmro(meth.__self__.__class__):
-           if cls.__dict__.get(meth.__name__) is meth:
-                return cls
-        meth = meth.__func__ # fallback to __qualname__ parsing
-    if isfunction(meth):
-        cls = getattr(getmodule(meth),
-                      meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
-        if isinstance(cls, type):
-            return cls
-    return None # not required since None would have been implicitly returned anyway
-
-
-def _lock_fieldnames_class(object_type: Type[Any], field_names:Tuple[str]=None):
-    """
-    Utility method to lock the possible fields of a class to the arguments declared in the constructor
-
-    :param object_type:
-    :param field_names: optional explicit list of field names
-    :return:
-    """
-
-    if not field_names:
-
-        # 1. Find the constructor
-        # extract unique constructor __init__
-        constructor = get_constructor(object_type)
-
-        # extract the __init__ signature
-        s = signature(constructor)
-
-        field_names = s.parameters.keys()
-
-    # now lock the names of fields : no additional field can be created on this object anymore
-    object_type.__slots__ = field_names
+# def _lock_fieldnames_class(object_type: Type[Any], field_names:Tuple[str]=None):
+#     """
+#     Utility method to lock the possible fields of a class to the arguments declared in the constructor
+#
+#     :param object_type:
+#     :param field_names: optional explicit list of field names
+#     :return:
+#     """
+#
+#     if not field_names:
+#
+#         # 1. Find the constructor
+#         # extract unique constructor __init__
+#         constructor = get_constructor(object_type)
+#
+#         # extract the __init__ signature
+#         s = signature(constructor)
+#
+#         field_names = s.parameters.keys()
+#
+#     # now lock the names of fields : no additional field can be created on this object anymore
+#     object_type.__slots__ = field_names
