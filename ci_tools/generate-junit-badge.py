d128d8d239d0359287c40f65199966b5e2ced8d3
Sylvain MARIE <sylvain.marie@se.com>
2019-04-17 18:48:26 +0200

    Improved unit test badge generation method to display number of tests

--- ci_tools/generate-junit-badge.py
+++ ci_tools/generate-junit-badge.py
@@ -1,28 +1,70 @@
+import sys
+try:
+    # python 3
+    from urllib.parse import quote_plus
+except ImportError:
+    # python 2
+    from urllib import quote_plus
+
 import requests
 import shutil
 from os import makedirs, path
 import xunitparser
 
 
-def download_badge(junit_xml: str='reports/junit/junit.xml', dest_folder: str='reports/junit'):
+class TestStats(object):
+    def __init__(self, success_percentage, success, runned, skipped):
+        self.success_percentage = success_percentage
+        self.success = success
+        self.runned = runned
+        self.skipped = skipped
 
-    makedirs(dest_folder, exist_ok=True)
 
-    # read the junit test file
+def get_test_stats(junit_xml='reports/junit/junit.xml'  # type: str
+                   ):
+    # type: (...) -> TestStats
+    """
+    read the junit test file and extract the success percentage
+    :param junit_xml: the junit xml file path
+    :return: the success percentage (an int)
+    """
     ts, tr = xunitparser.parse(open(junit_xml))
-    runned = tr.testsRun
+    skipped = len(tr.skipped)
+    runned = tr.testsRun - skipped
     failed = len(tr.failures)
+    success = runned - failed
 
-    success_percentage = round((runned - failed) * 100 / runned)
-    if success_percentage < 50:
+    success_percentage = round(success * 100 / runned)
+
+    return TestStats(success_percentage, success, runned, skipped)
+
+
+def download_badge(test_stats,                  # type: TestStats
+                   dest_folder='reports/junit'  # type: str
+                   ):
+    """
+    Downloads the badge corresponding to the provided success percentage, from https://img.shields.io.
+
+    :param test_stats:
+    :param dest_folder:
+    :return:
+    """
+    if not path.exists(dest_folder):
+        makedirs(dest_folder)  # , exist_ok=True) not python 2 compliant
+
+    if test_stats.success_percentage < 50:
         color = 'red'
-    elif success_percentage < 75:
+    elif test_stats.success_percentage < 75:
         color = 'orange'
-    elif success_percentage < 90:
+    elif test_stats.success_percentage < 90:
         color = 'green'
     else:
         color = 'brightgreen'
-    url = 'https://img.shields.io/badge/tests-' + str(success_percentage) + '%25-' + color + '.svg'
+
+    left_txt = "tests"
+    # right_txt = "%s%%" % test_stats.success_percentage
+    right_txt = "%s/%s" % (test_stats.success, test_stats.runned)
+    url = 'https://img.shields.io/badge/%s-%s-%s.svg' % (left_txt, quote_plus(right_txt), color)
 
     dest_file = path.join(dest_folder, 'junit-badge.svg')
 
@@ -35,5 +77,19 @@ def download_badge(junit_xml: str='reports/junit/junit.xml', dest_folder: str='r
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    download_badge()
+    # Execute only if run as a script.
+    # Check the arguments
+    assert len(sys.argv[1:]) == 1, "a single mandatory argument is required: <threshold>"
+    threshold = float(sys.argv[1])
+
+    # First retrieve the success percentage from the junit xml
+    test_stats = get_test_stats()
+
+    # Validate against the threshold
+    print("Success percentage is %s%%. Checking that it is >= %s" % (test_stats.success_percentage, threshold))
+    if test_stats.success_percentage < threshold:
+        raise Exception("Success percentage %s%% is strictly lower than required threshold %s%%"
+                        "" % (test_stats.success_percentage, threshold))
+
+    # Download the badge
+    download_badge(test_stats)
