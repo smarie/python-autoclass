38ce54c2b727efd5c046e64fa9fee82e3c6b38bc
Sylvain MARIE <sylvain.marie@se.com>
2019-04-17 18:49:07 +0200

    Updated travis configuration and tools

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -11,4 +11,19 @@ cleanup() {
 }
 
 trap "cleanup" INT TERM EXIT
-pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./autoclass -v autoclass/tests/
+
+#if hash pytest 2>/dev/null; then
+#    echo "pytest found"
+#else
+#    echo "pytest not found. Trying py.test"
+#fi
+
+# First the raw for coverage
+echo -e "\n\n****** Running tests ******\n\n"
+if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
+   # full
+   python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./autoclass -v autoclass/tests/
+else
+   # faster - skip coverage and html report
+   python -m pytest --junitxml=reports/junit/junit.xml -v autoclass/tests/
+fi
